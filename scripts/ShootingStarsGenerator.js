69bd5b3906fd53d1abfced37f31518cd520ac01c
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2020-09-03 21:51:46 -0400

    Repurposed methods
    Changed generate stars method to generate 4 children for each parent upon selection, and added functionality to delete all children on deselection.

--- scripts/ShootingStarsGenerator.js
+++ scripts/ShootingStarsGenerator.js
@@ -1,13 +1,29 @@
-var interval;
 
 function GenerateStars(){
-    interval = setInterval(function(){
-        var star = document.createElement("li");
-        star.classList.add("shooting-star");
-        document.getElementById("StarListParents").appendChild(star);
-    }, 1000);
+    for(i = 0; i < 9; i++){
+        GenerateChildren(i);
+    }
 }
 
-function StopStarGeneration(){
-    clearInterval(interval);
+function GenerateChildren(index){
+    for(var i = 0; i < 4; i++)
+    {
+        var star = document.createElement("div");
+        star.innerHTML = '&bigstar;';
+        star.classList.add("shooting-star-spawn");
+        document.getElementById("StarListParents").children[index].appendChild(star);
+    }
+}
+
+function DeleteStars(){
+    for(i = 0; i < 9; i++){
+        DeleteChildren(i);
+    }
+}
+
+function DeleteChildren(index){
+    for(var i = 0; i < 4; i++)
+    {
+        document.getElementById("StarListParents").children[index].children[0].remove();
+    }
 }
